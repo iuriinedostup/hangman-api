38e3806fcf4fc61d3275a2698d9631fe542abe93
Yuriy Nedostup <yuriy.nedostup@altexsoft.com>
2014-12-03 17:51:23 +0200

    feature-change-structure: changed app structure

--- api/web/index.php
+++ api/web/index.php
@@ -2,11 +2,11 @@
 
 define('DS', DIRECTORY_SEPARATOR);
 define('BASE_PATH', __DIR__);
-define('APP_PATH', BASE_PATH . DS . 'Src/Application');
+define('APP_PATH', realpath(BASE_PATH . DS . '..' . DS . 'Src/Application'));
 define('EXT','.php');
 
-if (is_readable('autoloader.php')) {
-    require_once 'autoloader.php';
+if (is_readable('../autoloader.php')) {
+    require_once '../autoloader.php';
 }
 
 use Src\Library\Core\FrontController;
