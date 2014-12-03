8368364f02955600e3ab405a8641e6f656b61f11
Yuriy Nedostup <yuriy.nedostup@gmail.com>
2014-12-04 00:17:33 +0200

    feature-init-tests: test demo API object function

---
+++ api/tests/Src/Application/APIObjects/DemoAPIObjectTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tests\Src\Application\APIObjects;
+
+
+
+use Src\Application\APIObjects\DemoAPIObject;
+use Src\Library\ApplicationConst;
+use Src\Library\Core\Request\Request;
+use Src\Library\Core\Response\Response;
+
+class DemoAPIObjectTest extends \PHPUnit_Framework_TestCase
+{
+    protected $_request;
+    protected $_response;
+
+    public function setUp()
+    {
+        $this->_request = new Request();
+        $this->_response = new Response();
+    }
+
+    public function testIndex()
+    {
+        $demoAPIObject = new DemoAPIObject($this->_request, $this->_response);
+        $this->assertInstanceOf('Src\Library\Core\Classes\APIObject', $demoAPIObject);
+    }
+}
